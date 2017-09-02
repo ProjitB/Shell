89b4967ac5149e0b7761bd5335f75de577564f6c
Projit_B <projit1234@hotmail.com>
2017-09-02 16:53:30 +0530

    error handling

--- spec4.c
+++ spec4.c
@@ -1,30 +1,22 @@
 int convert_Token(char *inputString, char **result)
 {
-  char *  p    = strtok (inputString, " ");
+  char *temp  = strtok (inputString, " ");
   int n_spaces = 0, i;
 
-  /* split string and append tokens to 'result' */
-
-  while (p) {
-    result = realloc (result, sizeof(char*) * ++n_spaces);
-
+  while (temp) {
+    result = realloc (result, sizeof(char*) * (++n_spaces));
     if (result == NULL)
       exit (-1); /* memory allocation failed */
-
-    result[n_spaces-1] = p;
-
-    p = strtok (NULL, " ");
+    result[n_spaces - 1] = temp;
+    temp = strtok (NULL, " ");
   }
-
   /* realloc one extra element for the last NULL */
-
   result = realloc (result, sizeof(char*) * (n_spaces+1));
   result[n_spaces] = 0;
 
   //result has the strings.
-    
-  execvp(result[0], result);
-  
+  int status = execvp(result[0], result);
+  if(status == -1) printf("Couldn't execute \'%s\'\n", result[0]);
   return n_spaces;
 }
 
--- test1/a.c
+++ test1/a.c
@@ -3,7 +3,8 @@
 int main()
 {
     int num;
-    scanf("Enter your name: %d", &num);
+    printf("Enter your name: ");
+    scanf("%d", &num);
     printf("Fuck this shit\n");
     return 0;
 }
